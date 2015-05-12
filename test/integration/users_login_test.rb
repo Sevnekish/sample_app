c86e4758150afd53fc09150b0642efbcf2b64be7
Nikita Khmelevsky <sevnekish@gmail.com>
2015-05-12 22:52:42 +0300

    Login upon signup.Logging out

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "login with invalid information" do
     # Visit the login path
     get login_path
@@ -16,6 +20,32 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     # Verify that the flash message doesn’t appear on the new page
     assert flash.empty?
+  end
 
+  test "login with valid inforamtion followed by logout" do
+    # Visit the login path
+    get login_path
+    # Post valid information to the sessions path
+    post login_path, session: { email: @user.email, password: 'password' }
+    assert is_logged_in?
+    # Check the right redirect target
+    assert_redirected_to @user
+    # Actually visit the target page
+    follow_redirect!
+    assert_template 'users/show'
+    # Verify that the login link disappears
+    assert_select "a[href=?]", login_path, count: 0
+    # Verify that a logout link appears
+    assert_select "a[href=?]", logout_path
+    # Verify that a profile link appears
+    assert_select "a[href=?]", user_path(@user)
+
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
