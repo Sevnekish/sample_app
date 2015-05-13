e69e6256f4b54d45cd893d24924a949f55924708
Nikita Khmelevsky <sevnekish@gmail.com>
2015-05-13 11:57:59 +0300

    Finish Log in/Log out

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -51,4 +51,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
+
+  test "login with remembering" do
+      log_in_as(@user, remember_me: '1')
+      # Inside tests the cookies method doesn’t work with symbols as keys
+      # cookies[:remember_token] is always nil
+      # Cookies does work with string keys:
+      assert_not_nil cookies['remember_token']
+  end
+
+  test "login without remembering" do
+      log_in_as(@user, remember_me: '0')
+      assert_nil cookies['remember_token']
+  end
 end
