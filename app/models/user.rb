aba701aef225905ed39f34d48096e55046a1a068
Nikita Khmelevsky <sevnekish@gmail.com>
2015-05-14 22:55:10 +0300

    Account activations resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase}
-  validates :name,  presence: true, length: { maximum: 50 }
+
+  attr_accessor :remember_token, :activation_token
+
+  before_save   :downcase_email
+  before_create :create_activation_digest
+
+
   VALID_EMAIL_REGEX = /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  validates :name,  presence: true, length: { maximum: 50 }
+  
   validates :email, presence: true, length: { maximum: 255 },
                                     format: { with: VALID_EMAIL_REGEX},
                                     uniqueness: { case_sensitive: false }
@@ -59,6 +65,18 @@ class User < ActiveRecord::Base
     # else
     #   BCrypt::Password.new(remember_digest).is_password?(remember_token)
     # end
-
   end
+
+  private
+
+    # Converts email to all lower-case
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # Creates and assigns the actiovation token and digest
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
