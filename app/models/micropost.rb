ceae1b449fd4a514d3e940c72af6df770adda12a
Nikita Khmelevsky <sevnekish@gmail.com>
2015-05-19 20:34:01 +0300

    Basic image upload

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,7 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   # Used to set the default order in which elements are retrieved from the database.
   default_scope -> { order(created_at: :desc) } # order('created_at DESC')
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
