dbb1e03078a343dbfb7c56554726cd62f1dd4972
Nikita Khmelevsky <sevnekish@gmail.com>
2015-05-19 11:32:59 +0300

    Dependent: destroy

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,7 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
+  # Used to set the default order in which elements are retrieved from the database.
+  default_scope -> { order(created_at: :desc) } # order('created_at DESC')
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
