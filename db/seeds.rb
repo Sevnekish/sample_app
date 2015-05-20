b1ce4c664c3f38d714090c119c4f42922ab439d3
Nikita Khmelevsky <sevnekish@gmail.com>
2015-05-20 15:49:58 +0300

    Stats and a follow form

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@
 
 # The create! method is just like the create method, except it
 # raises an exception for an invalid user rather than returning false. 
+
+# Users
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
@@ -28,8 +30,18 @@ User.create!(name:  "Example User",
                activated_at: Time.zone.now)
 end
 
+
+# Microposts
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
